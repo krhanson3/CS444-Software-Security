8bcab0f554da252b264aff0e00f7d531da35b43a
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2025-11-11 11:14:25 -0600

    Delete Fall2025-Project3-krhanson3 directory

--- Fall2025-Project3-krhanson3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fall2025_Project3_krhanson3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
